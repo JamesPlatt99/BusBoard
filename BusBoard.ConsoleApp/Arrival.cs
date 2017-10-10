74e257a286fe9bac968ed5ac39bd9471fd332351
James Platt <james.platt@neptune.com>
2017-10-10 14:14:05 +0100

    The program now accepts a user input for the StopPoint ID and responds with the times of the next 5 busses.

---
+++ BusBoard.ConsoleApp/Arrival.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusBoard.ConsoleApp
+{
+    class Arrival
+    {
+        public string id { get; set; }
+        public string vehicleID { get; set; }
+        public string direction { get; set; }
+        public DateTime expectedArrival { get; set; }
+    }
+}
--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -1,15 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Deserializers;
+using Newtonsoft.Json;
 
 namespace BusBoard.ConsoleApp
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
+        private const string AppId = "5f30eddc";
+        private const string ApiKey = "f1c6c957988fdacfe019e95f21d72abc";
+
+        private static void Main(string[] args)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            Console.Write("Please enter the stop point id: ");
+            string busStop = Console.ReadLine();
+            List<Arrival> arrivals = GetResponse(busStop);
+            arrivals = arrivals.OrderBy(n => n.expectedArrival).ToList();
+            Console.WriteLine();
+            Console.WriteLine("The next 5 busses are scheduled to arrive at the following times:");
+            for(int i = 0;i<5;i++)
+            {
+                Console.WriteLine("{0:g}",arrivals[i].expectedArrival);
+            }
+            Console.ReadLine();
+
+        }
+        private static List<Arrival> GetResponse(string busStop)
+        {
+            RestClient client = new RestClient("https://api.tfl.gov.uk/");
+            RestRequest request = new RestRequest("StopPoint/{ids}/Arrivals", Method.GET);
+            request.AddUrlSegment("app_id", AppId);
+            request.AddUrlSegment("app_key", ApiKey);
+            request.AddUrlSegment("ids", busStop);
+            List<Arrival> arrivals = new List<Arrival>();
+            IRestResponse response = new RestResponse();
+            response = client.Execute(request);
+
+            arrivals = GetArrival(response.Content);
+            
+            return arrivals;
+        }
+
+        private static List<Arrival> GetArrival(string content)
+        {
+            List<Arrival> arrivals = JsonConvert.DeserializeObject<List<Arrival>>(content);
+
+            return arrivals;
+        }
     }
-  }
+
 }
